f92e4c44292bc768539faf98e46b599976cf1c46
leromayagg <leromayagg@gmail.com>
2025-05-19 22:52:35 -0300

    First Steps in Java with Spring Boot 3.4.0

---
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/leromayagg/model/Greeting.java
@@ -0,0 +1,5 @@
+package br.com.leromayagg.model;
+
+public record Greeting(Long id, String content) {
+
+}
---
+++ rest-with-spring-boot-and-java-erudio/src/test/java/br/com/leromayagg/StartupTest.java
@@ -0,0 +1,13 @@
+package br.com.leromayagg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
